a358bc785bc10b6cfa658a8b4b927a0a25d78d4d
eichtuou <22988667+eichtuou@users.noreply.github.com>
2024-07-09 13:02:00 -0400

    finished upgrading code

--- analyzeTDDFT/analyzeTDDFT.py
+++ analyzeTDDFT/analyzeTDDFT.py
@@ -1,7 +1,5 @@
 """This script gets excitations of interest from a log file of a TD-DFT
 calculation from Gaussian09.
-
-Run as: python analyzeTDDFT.py file.log
 """
 
 import argparse
@@ -17,17 +15,18 @@ def parse_input_params():
 
     Returns
     ----------
-    args
+    args : obj
+        Argparse object containing parsed input parameters.
     """
     parser = argparse.ArgumentParser(
         description="Analyze TD-DFT calculations from Gaussian09 log files.")
     parser.add_argument('logfile',
-                        help="Path to the Gaussian09 log file.")
-    parser.add_argument('-nm_min', type=int, default=50,
+                        help="Path to Gaussian09 log file.")
+    parser.add_argument('-nm_min', metavar='', type=float, default=50,
                         help="Minimum excitation wavelength in nm.")
-    parser.add_argument('-nm_max', type=int, default=150,
+    parser.add_argument('-nm_max', metavar='', type=float, default=150,
                         help="Maximum excitation wavelength in nm.")
-    parser.add_argument('-osc_min', type=float, default=0.01,
+    parser.add_argument('-osc_min', metavar='', type=float, default=0.01,
                         help="Minimum oscillator strength.")
     args = parser.parse_args()
 
@@ -35,7 +34,7 @@ def parse_input_params():
 
 
 def get_excited_states(line, line_number, wavenum_min, wavenum_max,
-               oscillator_min, excited_states):
+                       oscillator_min, excited_states):
     """Get excited states with specific oscillator strength.
 
     Parameters
@@ -44,14 +43,15 @@ def get_excited_states(line, line_number, wavenum_min, wavenum_max,
         Line from file.
     line_number : int
         Line number/index in file.
-    nm_min : float
+    wavenum_min : float
         Minimum excitation wavelength in nm.
-    nm_max : float
+    wavenum_max : float
         Maximum excitation wavelength in nm.
-    fosc_min : float
+    oscillator_min : float
         Minimum oscillator strength.
-    states : dict
-        Dictionary containing excited states of interest.
+    excited_states : dict
+        Dictionary containing excited states of interest and their respective
+        line number.
 
     Returns
     ----------
@@ -62,25 +62,31 @@ def get_excited_states(line, line_number, wavenum_min, wavenum_max,
     oscillator = float(line[8].replace('f=', ''))
 
     if wavenum >= wavenum_min and wavenum <= wavenum_max \
-        and oscillator >= oscillator_min:
+            and oscillator >= oscillator_min:
         excited_states[int(line[2].replace(':', ''))] = line_number
 
     return None
 
 
 def read_logfile(logfile, wavenum_min=50, wavenum_max=150, oscillator_min=0.01):
-    """Read log file and identify lines with excitation information.
+    """Read log file and identify lines with excitated states of interest.
 
     Parameters
     ----------
-    logfile :
-    wavenum_min :
-    wavenum_max :
-    oscilaltor_min :
+    logfile : str
+        Name of logfile.
+    wavenum_min : float
+        Minimmum excitation wavelength in nm.
+    wavenum_max : float
+        Maximum excitation wavelength in nm.
+    oscillator_min : float
+        Minimum oscillator strength.
 
     Returns
     ----------
-    excited_states :
+    excited_states : dict
+        Dictionary containing excited states of interest and their respective
+        line number.
     """
     excited_states = {}
 
@@ -89,23 +95,24 @@ def read_logfile(logfile, wavenum_min=50, wavenum_max=150, oscillator_min=0.01):
             line = line.strip()
             if 'Excited State' in line:
                 get_excited_states(line, line_number, wavenum_min, wavenum_max,
-                           oscillator_min, excited_states)
+                                   oscillator_min, excited_states)
 
     return excited_states
 
 
 def get_orbitals(line, orbitals):
-    """Get orbital contributions to excited state and update lists.
+    """Get orbital contributions to excited state.
 
     Parameters
     ----------
-    line :
-    index :
+    line : str
+        Line from file.
+    orbitals : dict
+        Dictionary containing the excited state orbitals.
 
     Returns
     ----------
-
-
+    None
     """
     line = line.split()
     orbitals["contributions"].extend([f"{(2 * float(line[3]) ** 2):.2f}"])
@@ -115,27 +122,31 @@ def get_orbitals(line, orbitals):
 
     return None
 
-def write_output(logfile, indeces):
+
+def write_output(logfile, excited_states):
     """Write output file with excited states and orbital contributions.
 
     Parameters
     ----------
-    logfile :
-    index :
+    logfile : str
+        Name of logfile.
+    excited_states : dict
+        Dictionary containing excited states of interest and their respective
+        line number.
 
     Returns
     ----------
     None
     """
-    indeces = indeces.values()
+    line_numbers = list(excited_states.values())
 
     with open(logfile[:-4] + '_excitations.dat', 'w') as outfile:
-        for index in indeces:
-            line = linecache.getline(logfile, index).strip()
+        for line_number in line_numbers:
+            line = linecache.getline(logfile, line_number).strip()
             line = line.split()
 
-            outfile.write(f"Excited State {line[2]}\n"
-                          f"    nm = {line[6]}    f = {line[8][2:]}\n")
+            text_buffer = (f"Excited State {line[2]}\n"
+                           f"    nm = {line[6]}    f = {line[8][2:]}\n")
 
             orbitals = {
                 "contributions": [],
@@ -144,21 +155,27 @@ def write_output(logfile, indeces):
                 "coefficients": []
             }
 
-            for i in range(1, 100):
-                next_line = linecache.getline(logfile, index + i).strip()
+            key_index = line_numbers.index(line_number)
+            if key_index < len(line_numbers) - 1:
+                max_iter = line_numbers[key_index +
+                                        1] - line_numbers[key_index]
+            else:
+                max_iter = 500
+
+            for i in range(1, max_iter):
+                next_line = linecache.getline(logfile, line_number + i).strip()
                 if '->' in next_line:
                     get_orbitals(next_line, orbitals)
                 if 'Excited State' in next_line:
                     break
 
-            print(orbitals)
-            for i in range(len(orbitals)):
-                #outfile.write(f"    {orbs_occupied[i]} -> {virtual[i]}    {coefficients[i]}    "
-                #              f"contr = {contributions[i]}\n"
-                print(f"    {orbitals["occupied"]} -> {orbitals["virtual"]}    {orbitals["coefficients"]}    "
-                              f"contr = {orbitals["contributions"]}\n")
+            for i in range(0, len(orbitals["contributions"])):
+                text_buffer += "    " \
+                    f"{orbitals["occupied"][i]} -> {orbitals["virtual"][i]}    "\
+                    f"{orbitals["coefficients"][i]}    "\
+                    f"contr = {orbitals["contributions"][i]}\n"
 
-            #outfile.write('\n')
+            outfile.write(text_buffer + "\n")
 
     return None
 
@@ -168,8 +185,9 @@ def main():
     args = parse_input_params()
 
     try:
-        states = read_logfile(args.logfile, args.nm_min, args.nm_max, args.osc_min)
-        write_output(args.logfile, states)
+        excited_states = read_logfile(
+            args.logfile, args.nm_min, args.nm_max, args.osc_min)
+        write_output(args.logfile, excited_states)
 
     except Exception as e:
         print(f"Error: {e}")
@@ -177,4 +195,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
