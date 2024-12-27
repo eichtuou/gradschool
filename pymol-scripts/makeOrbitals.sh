b801e62436baff221f40987acde9cf78664a9c6f
eichtuou <22988667+eichtuou@users.noreply.github.com>
2024-12-27 15:55:27 -0500

    Renamed pymol directory and updated script to python 3.

--- pymol-scripts/makeOrbitals.sh
+++ pymol-scripts/makeOrbitals.sh
@@ -1,14 +1,6 @@
-#!/bin/bash
-#
-# Author: Jessica M. Gonzalez-Delgado
-#		  North Carolina State University
-#
-# This script sets up cube files to be read by a pymol script that 
-# generates png files of the orbitals of a molecule, then organizes 
+# This script sets up cube files to be read by a pymol script that
+# generates png files of the orbitals of a molecule, then organizes
 # the png files in directories by isovalues of the surfaces.
-#
-# Run as: ./mkorbs.sh
-#
 
 # molecule name
 cname="febpycn4s_monoCA_mo"
@@ -24,7 +16,7 @@ do
 done
 
 # call pymol script
-python2.7 pymol_orbs.py
+py pymol_orbs.py
 
 # organize png files by iso values
 declare -a iso=("0.01" "0.02" "0.03" "0.04" "0.05" "0.06" "0.008")
